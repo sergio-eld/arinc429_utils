c7e4f64d58ea5b99c48efebe4b3a250ae3939b8f
Selyan <n.selyanin@uwca.ru>
2021-06-18 14:06:08 +0500

    Added more tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,7 +3,226 @@
 
 #include <gtest/gtest.h>
 
+#include <limits>
+#include <cmath>
+
 namespace elda = eld;
+
+TEST(ElementaryTests, SingleUint32DataInHead)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 1, 10, uint32_t> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const auto exp_data_0 = uint32_t(std::pow(2, 10) - 1);
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, SingleUint32DataInCenter)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 11, 20, uint32_t> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const auto exp_data_0 = uint32_t(std::pow(2, 10) - 1);
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, SingleUint32DataInTail)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 21, 32, uint32_t> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const auto exp_data_0 = uint32_t(std::pow(2, 10) - 1);
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, SingleInt32DataInHead)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 1, 10, int32_t> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const auto exp_data_0 = - int32_t(std::pow(2, 9) - 1);
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, SingleInt32DataInCenter)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 11, 20, int32_t> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const auto exp_data_0 = - int32_t(std::pow(2, 9) - 1);
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, SingleInt32DataInTail)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 21, 32, int32_t> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const auto exp_data_0 = - int32_t(std::pow(2, 9) - 1);
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, SingleDoubleDataInHead)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 1, 10, double, std::ratio<1, 100>> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const double exp_data_0 = 3.33;
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, SingleDoubleDataInCenter)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 11, 20, double, std::ratio<1, 100>> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const double exp_data_0 = 3.33;
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, SingleDoubleDataInTail)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 21, 32, double, std::ratio<1, 100>> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const double exp_data_0 = 3.33;
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, FullCamapcityDataUint)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 1, 32, uint32_t> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const auto exp_data_0 = uint32_t(std::pow(2, 32) - 1);
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, FullCamapcityDataInt)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 1, 32, int32_t> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const auto exp_data_0 = - int32_t(std::pow(2, 31) - 1);
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, FullCamapcityDataDouble)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 1, 32, double, std::ratio<3, 100>> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0>;
+
+    const auto exp_data_0 = -(std::pow(2, 31) - 1) * 0.03;
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+}
+
+TEST(ElementaryTests, MultipleUint32Data)
+{
+    struct data_0 : elda::arinc429::data_descriptor<data_0, 1, 10, uint32_t> {};
+    struct data_1 : elda::arinc429::data_descriptor<data_1, 11, 20, uint32_t> {};
+    struct data_2 : elda::arinc429::data_descriptor<data_2, 21, 32, uint32_t> {};
+
+    using word_with_label_t =
+    elda::arinc429::word_generic<data_0, data_1, data_2>;
+
+    const auto exp_data_0 = uint32_t(std::pow(2, 10) - 1);
+    const auto exp_data_1 = 9;
+    const auto exp_data_2 = uint32_t(std::pow(2, 12) - 1);
+
+    word_with_label_t wordWithLabel{ 0 };
+
+    wordWithLabel.set<data_0>(exp_data_0);
+    wordWithLabel.set<data_1>(exp_data_1);
+    wordWithLabel.set<data_2>(exp_data_2);
+
+    EXPECT_EQ(exp_data_0, wordWithLabel.get<data_0>());
+    EXPECT_EQ(exp_data_1, wordWithLabel.get<data_1>());
+    EXPECT_EQ(exp_data_2, wordWithLabel.get<data_2>());
+}
 
 TEST(SetWordTest, ComplexWordWithNegDoubleData)
 {
@@ -46,7 +265,7 @@ TEST(SetWordTest, ComplexWordWithNegDoubleData)
     // EXPECT_EQ(exp_parity, wordWithLabel.get<parity>());
 }
 
-TEST(GetWordTest, DISABLE_ComplexWordWithNegDoubleData)
+TEST(GetWordTest, ComplexWordWithNegDoubleData)
 {
     struct label : elda::arinc429::data_descriptor<label, 1, 8, uint8_t>
     {
@@ -86,6 +305,8 @@ TEST(GetWordTest, DISABLE_ComplexWordWithNegDoubleData)
     wordWithLabel.set<data_2>(exp_data_2);
     wordWithLabel.set<state_matrix>(exp_state_matrix);
     wordWithLabel.set<parity>(exp_parity);
+
+    EXPECT_EQ(0xf3f21872, wordWithLabel.get_raw());
 }
 
 TEST(SetAndGetWordTests, Uint32DataWord)
