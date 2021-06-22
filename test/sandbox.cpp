f17a4593690fd4dd8c95711fcff9cf3b8895b0ec
s.kolesnik <7fosGbc9S>
2021-06-22 13:04:19 +0500

    #7 modified get_data_descriptor to use word directly. Added type traits. Implemented modifier class

--- test/sandbox.cpp
+++ test/sandbox.cpp
@@ -7,14 +7,26 @@ int main()
     static_assert(42 == sum, "");
 
     struct label {};
-    using label_name = eld::arinc429::data_descriptor<label, 1, 8, uint8_t>;
+    using label_descriptor = eld::arinc429::data_descriptor<label, 1, 8, uint8_t>;
 
-    using word_with_label_t = eld::arinc429::word_generic<label_name>;
+    using word_with_label_t = eld::arinc429::word_generic<label_descriptor>;
 
     word_with_label_t wordWithLabel{07};
 
     auto labelValue = wordWithLabel.get<label>();
     wordWithLabel.set<label>(011);
+
+    static_assert(std::is_same<uint8_t, eld::arinc429::traits::value_type_t<label_descriptor>>(), "");
+    static_assert(std::is_same<label_descriptor, eld::arinc429::traits::get_data_descriptor_t<label, word_with_label_t>>(), "");
+    static_assert(std::is_same<uint8_t, eld::arinc429::traits::value_type_t<eld::arinc429::traits::get_data_descriptor_t<label, word_with_label_t>>>(), "");
+
+
+    static_assert(std::is_same<uint8_t, typename eld::arinc429::modifier<label, word_with_label_t>::value_type>(), "");
+
+    eld::arinc429::modify<label>(wordWithLabel) += uint8_t(26);
+    auto label_modifier = eld::arinc429::modify<label>(wordWithLabel);
+
+    label_modifier += uint8_t(26);
 
     return 0;
 }
